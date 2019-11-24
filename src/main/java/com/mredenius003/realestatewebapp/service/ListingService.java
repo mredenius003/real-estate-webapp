595ec8b6b46a02dddaeed2753dd487b3d76f65b4
mredenius003 <mredenius003@gmail.com>
2019-11-23 20:30:17 -0800

    Added service layer for Users and Listings.

---
+++ src/main/java/com/mredenius003/realestatewebapp/service/ListingService.java
@@ -0,0 +1,63 @@
+package com.mredenius003.realestatewebapp.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mredenius003.realestatewebapp.model.Listing;
+import com.mredenius003.realestatewebapp.model.User;
+import com.mredenius003.realestatewebapp.repository.ListingRepository;
+import com.mredenius003.realestatewebapp.repository.UserRepository;
+
+@Service("userService")
+public class ListingService {
+
+    private final UserRepository userRepository;
+    private final ListingRepository listingRepository;
+
+    @Autowired
+    public ListingService(UserRepository userRepository, ListingRepository listingRepository) {
+        this.userRepository = userRepository;
+        this.listingRepository = listingRepository;
+    }
+
+    public Listing findByMls(long mls) {
+        return listingRepository.findByMls(mls);
+    }
+
+    public Set<Listing> findByCity(String city) {
+        return findByCity(city);
+    }
+
+    public Set<Listing> findByState(String state) {
+        return listingRepository.findByState(state);
+    }
+
+    public Set<Listing> findByZipcode(String zipcode) {
+        return listingRepository.findByZipcode(zipcode);
+    }
+
+    public Set<Listing> findByNumBedrooms(Integer numBedrooms) {
+        return listingRepository.findByNumBedrooms(numBedrooms);
+    }
+
+    public Set<Listing> findByNumBathrooms(Integer numBathrooms) {
+        return listingRepository.findByNumBathrooms(numBathrooms);
+    }
+
+    public Set<Listing> findByLotSize(Double lotSize) {
+        return listingRepository.findByLotSize(lotSize);
+    }
+
+    public Set<Listing> findListinsByUser(User user) {
+        Set<Listing> listings = new HashSet<Listing>();
+        user.getListings().forEach(listing -> listings.add(listing));
+        return listings;
+    }
+
+    public void saveListing(Listing listing) {
+        listingRepository.save(listing);
+    }
+}
---
+++ src/main/java/com/mredenius003/realestatewebapp/service/UserService.java
@@ -0,0 +1,30 @@
+package com.mredenius003.realestatewebapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.mredenius003.realestatewebapp.model.User;
+import com.mredenius003.realestatewebapp.repository.UserRepository;
+
+@Service("userService")
+public class UserService {
+
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+}
