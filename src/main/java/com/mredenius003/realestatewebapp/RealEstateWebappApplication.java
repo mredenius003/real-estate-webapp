581097a573f72a25e78a6282205acbdc2b42a757
mredenius003 <mredenius003@gmail.com>
2019-11-23 17:57:51 -0800

    Updated .gitignore, added model, and commented out unit tests.

--- src/main/java/com/mredenius003/realestatewebapp/RealEstateWebappApplication.java
+++ src/main/java/com/mredenius003/realestatewebapp/RealEstateWebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RealEstateWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RealEstateWebappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateWebappApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/mredenius003/realestatewebapp/model/Listing.java
@@ -0,0 +1,85 @@
+package com.mredenius003.realestatewebapp.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "listing")
+public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "mls")
+    @NotNull
+    private long mls;
+
+    @Column(name = "street")
+    @NotEmpty(message = "*Please provide a street.")
+    private String street;
+
+    @Column(name = "street2")
+    private String street2;
+
+    @Column(name = "city")
+    @NotEmpty(message = "*Please provide a city.")
+    private String city;
+
+    @Column(name = "state")
+    @NotEmpty(message = "*Please provide a state.")
+    private String state;
+
+    @Column(name = "zipcode")
+    @NotEmpty(message = "*Please provide a zipcode.")
+    private String zipcode;
+
+    @Column(name = "neighborhood")
+    private String neighborhood;
+    
+    @Column(name = "date")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @NotNull
+    private LocalDate dateListed;
+
+    @Column(name = "numBedroom")
+    @NotEmpty(message = "*Please provide the number of bedrooms.")
+    private Integer numBedrooms;
+    
+    @Column(name = "image")
+    private List<String> imagePaths;
+
+    @Column(name = "bathroom")
+    @NotEmpty(message = "*Please provide the number of bathrooms.")
+    private Integer numBathrooms;
+
+    @Column(name = "garageSize")
+    private Integer garageSize;
+
+    @Column(name = "sqaureFootage")
+    @NotEmpty(message = "*Please provide the square footage.")
+    private Integer squareFootage;
+
+    @Column(name = "lotSize")
+    @NotEmpty(message = "*Please provide the lot size in acres.")
+    private Double lotSize;
+
+    @Column(name = "description")
+    @NotEmpty(message = "*Please provide a description")
+    private String description;
+}
