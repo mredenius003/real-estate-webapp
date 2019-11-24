530f4cac23a8f4db9d098ef32e8e470738395d75
mredenius003 <mredenius003@gmail.com>
2019-11-23 16:19:14 -0800

    Initial commit.

---
+++ src/test/java/com/mredenius003/realestatewebapp/RealEstateWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mredenius003.realestatewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
